b9ad074772bab8a0baff272862107e4efa8bb891
onur1211 <onuralp.mete02@gmail.com>
2023-10-22 20:46:57 +0200

    Added authentication app with express and ejs

--- Lab2/NodeJSApp/main.js
+++ Lab2/NodeJSApp/main.js
@@ -1,1 +1,116 @@
-console.log("TEST")
+'use strict'
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var path = require('path');
+var session = require('express-session');
+
+var app = module.exports = express();
+
+// config
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// middleware
+app.use(express.urlencoded({ extended: false }))
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'shhhh, very secret'
+}));
+
+// Session-persisted message middleware
+app.use(function(req, res, next){
+  var err = req.session.error;
+  var msg = req.session.success;
+  delete req.session.error;
+  delete req.session.success;
+  res.locals.message = '';
+  if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
+  if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
+  next();
+});
+
+// Dummy database
+var users = {
+  admin: { name: 'admin', password: 'admin123' }
+};
+
+// Authenticate using our plain-object database
+function authenticate(name, pass, fn) {
+  if (!module.parent) console.log('Authenticating %s:%s', name, pass);
+  var user = users[name];
+  // query the users 'db' for the given username
+  if (!user) return fn(null, null)
+
+  if (user.name == name && user.password == pass) {
+    console.log("Authentication succeeded: ", name, pass);
+    return fn(null, user);
+  }
+  else {
+    console.log("Authentication failed: ", name, pass);
+    return fn(null, null);
+  }
+}
+
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = 'Access denied!';
+    res.redirect('/login');
+  }
+}
+
+app.get('/', function(req, res){
+  res.redirect('/login');
+});
+
+app.get('/restricted', restrict, function(req, res){
+  res.send('Wahoo! restricted area, click to <a href="/logout">logout</a>');
+});
+
+app.get('/logout', function(req, res){
+  // destroy the user's session to log them out
+  // will be re-created next request
+  req.session.destroy(function(){
+    res.redirect('/');
+  });
+});
+
+app.get('/login', function(req, res){
+  res.render('login');
+});
+
+app.post('/login', function (req, res, next) {
+  authenticate(req.body.username, req.body.password, function(err, user){
+    if (err) return next(err);
+    if (user) {
+      // Regenerate session when signing in
+      // to prevent fixation
+      req.session.regenerate(function(){
+        // Store the user's primary key
+        // in the session store to be retrieved,
+        // or in this case the entire user object
+        req.session.user = user;
+        req.session.success = 'Authenticated as ' + user.name
+          + ' click to <a href="/logout">logout</a>. '
+          + ' You may now access <a href="/restricted">/restricted</a>.';
+        res.redirect('back');
+      });
+    } else {
+      req.session.error = 'Authentication failed, please check your '
+        + ' username and password.'
+        + ' (use "admin" and "admin123")';
+      res.redirect('/login');
+    }
+  });
+});
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}
